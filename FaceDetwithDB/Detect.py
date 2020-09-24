fd46cfb787b69c6085570565d74904579b940386
root <leduchoa911@gmail.com>
2020-09-24 23:37:36 +0700

    detect ok

---
+++ FaceDetwithDB/Detect.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+
+face_cas = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+cap = cv.VideoCapture(0)
+while (True):
+    ret, frame = cap.read()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cas.detectMultiScale(gray)
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv.imshow("Detect Face", frame)
+    if cv.waitKey(0) & 0xFF == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
