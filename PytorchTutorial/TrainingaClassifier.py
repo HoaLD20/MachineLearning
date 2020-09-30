fa73c7804c434a50e6dfb86f65d11530b87397a7
HoaLD <leduchoa911@gmail.com>
2020-09-30 15:19:32 +0700

    Scikit learn tutorial_2 test

--- PytorchTutorial/TrainingaClassifier.py
+++ PytorchTutorial/TrainingaClassifier.py
@@ -6,10 +6,10 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./Document', train=True, download=True, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='./Data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./Document', train=False, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./Data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
